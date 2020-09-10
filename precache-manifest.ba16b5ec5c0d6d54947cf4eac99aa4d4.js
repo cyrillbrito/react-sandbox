704118e06076085209c957e2f52886a61c9b36b0
cyrillbrito <cyrillbrito@users.noreply.github.com>
2020-09-10 20:10:21 +0000

    deploy: a91e2de300fa92f2fd2e3ef2fe4fc35706df933f

---
+++ precache-manifest.ba16b5ec5c0d6d54947cf4eac99aa4d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848c3966c4b9d29c3f488f20a3c76298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a34f82be21934dedbe89",
+    "url": "/static/css/main.93dfdd95.chunk.css"
+  },
+  {
+    "revision": "c260eb8b5f6d8bec7df3",
+    "url": "/static/js/2.92bb042c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.92bb042c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34f82be21934dedbe89",
+    "url": "/static/js/main.7f854030.chunk.js"
+  },
+  {
+    "revision": "5949885017d7d3460b0c",
+    "url": "/static/js/runtime-main.38ad96b8.js"
+  }
+]);
