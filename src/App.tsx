67592594d9d31e5afee69350e03a49d5ddc15938
Cyrill Brito <cyrill.brito@gmail.com>
2020-09-10 19:56:20 +0100

    change branch name on the workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Tile } from './Tile';
-import { Board } from './Board';
+import { Board } from './board/Board';
 
 export default class extends Component {
 
