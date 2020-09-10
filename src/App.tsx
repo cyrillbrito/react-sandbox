3eb9ef04e62ec0bd91b04134e1ae1402ef8e3367
Cyrill Brito <cyrill.brito@gmail.com>
2020-09-10 20:03:58 +0100

    Switch to a yarn workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Tile } from './Tile';
 import { Board } from './board/Board';
 
 export default class extends Component {
