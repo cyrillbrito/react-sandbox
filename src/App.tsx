c2a33248eef66c09c58581c63928fc99cfc0bc55
Cyrill Brito <cyrill.brito@gmail.com>
2020-09-11 23:56:01 +0100

    Added a basic test

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,12 @@ import { Layout, Menu } from 'antd';
 
 export class App extends Component {
 
-
   render() {
-
-
     const { Header, Content } = Layout;
     return (
       <Layout className="layout">
         <Header>
-          <div className="logo" />
+          <div className="logo">React Sandbox</div>
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
